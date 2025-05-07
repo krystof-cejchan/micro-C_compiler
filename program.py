5604c8c2e3093100c8f7a0a6ef7776856f049564
Kryštof Čejchan <kcejchan@koop.cz>
2025-05-07 11:18:03 +0200

    tests

--- program.py
+++ program.py
@@ -1,8 +1,32 @@
 def __mikroc_main():
-    cislo = 9
-    print(f"%5i" % (cislo), end="")
-    c = 7
-    a = 8
+    print(f"\n              ASCII tabulka\n\n    ", end="")
+    i = 0
+    while (i < 16):
+        print(f" %X" % (i), end="")
+        i += 1
+    print(f"\n   ", end="")
+    i = 0
+    while (i < 35):
+        print(f"-", end="")
+        i += 1
+    i = 32
+    while (i < 128):
+        print(f"\n %X|" % (i), end="")
+        j = i
+        while (j < (i + 16)):
+            if (j < 127):
+                print(f" %c" % (j), end="")
+            else:
+                print(f"  ", end="")
+            j += 1
+        print(f" |", end="")
+        i += 16
+    print(f"\n   ", end="")
+    i = 0
+    while (i < 35):
+        print(f"-", end="")
+        i += 1
+    print(f"\n\n", end="")
     pass
 
 if __name__ == '__main__':
